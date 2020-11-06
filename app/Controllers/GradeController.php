4859fdff7084b10152a7165a409101ddbb3d12af
emilija26 <74071875+emilija26@users.noreply.github.com>
2020-11-06 23:18:06 +0100

    Create GradeController.php

---
+++ app/Controllers/GradeController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\Controller;
+use App\Models\Student;
+use App\Models\SchoolBoard;
+use App\Models\Grade;
+
+class GradeController extends Controller
+{
+    /**
+     * Display all students
+     *
+     * @param Request $request
+     */
+    public function index(Request $request)
+    {
+        $student = Student::findOne($request->get('id'));
+        $grades = Student::getGrades($request->get('id'));
+        return $this->render('grades/index.html', ['student' => $student, 'grades' => $grades]);
+    }
+
+    /**
+     * Display all students
+     *
+     * @param Request $request
+     */
+    public function store(Request $request)
+    {
+        $studentId = $request->get('id_student');
+        $gradeCount = Student::gradeCount($studentId);
+        if (intval($gradeCount['count']) < 4) {
+            $grade = new Grade([
+                'value' => intval($request->get('value')),
+                'id_student' => intval($studentId)
+            ]);
+        }
+        return Response::redirect("students/{$studentId}/grades");
+    }
+}
