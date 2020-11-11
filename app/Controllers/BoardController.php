c4be127061d2238bfaaaae77273c40b4f9aa7e85
emilija26 <74071875+emilija26@users.noreply.github.com>
2020-11-11 21:38:27 +0100

    Update BoardController.php

--- app/Controllers/BoardController.php
+++ app/Controllers/BoardController.php
@@ -7,7 +7,7 @@ use App\Models\SchoolBoard;
 class BoardController extends Controller
 {
 	
-    public function store(Request $request)
+    public function store(Request $request)    //get name on school board and type (CSM or CSMB)
     {
 		$name = $this->input->get("name");
 		$type = $this->input->get("type");
